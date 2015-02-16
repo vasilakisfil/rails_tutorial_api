dd31cb23d7ca78e9d104e656c3a3a9abf2039195
Filippos Vasilakis <vasilakisfil@gmail.com>
2015-02-16 15:42:31 +0100

    fix tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -48,12 +48,16 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
+
   config.before(:suite) do
-    begin
-      DatabaseCleaner.start
-      FactoryGirl.lint
-    ensure
-      DatabaseCleaner.clean
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
     end
   end
+
 end
